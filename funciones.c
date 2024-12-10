463e32ba00a09a97abb7ff0eb623803e23a5052c
Leandro <jordanspozo@gmail.com>
2024-12-10 13:26:28 -0500

    inicio

---
+++ funciones.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "funciones.h"
+int c;
+void registrarLibros(struct Libro libros[20]){
+    printf("\n");
+    printf("\n.:INGRESO DE LIBROS:.");
+    printf("\nIngrese Cuantos Libros desea Ingresar");scanf("%i", &c);
+
+    for (int i = 0; i < c; i++)
+    {
+        printf("\n");
+        printf("\nLibro %i. ", i + 1);
+        
+
+        printf("\n");
+    }
+    
+
+    printf("\n");
+}
+
+void mostrarLibros(libros){
+    printf("\n");
+
+
+
+    printf("\n");
+}
+void buscarLibroId(libros){
+    printf("\n");
+
+
+
+    printf("\n");
+}
+void buscarLibroTitulo(libros){
+    printf("\n");
+
+
+
+    printf("\n");
+}
+void ActualizarLibro(libros){
+    printf("\n");
+
+
+
+    printf("\n");
+}
+void EliminarLibro(libros){
+    printf("\n");
+
+
+
+    printf("\n");
+}
--- main.c
+++ main.c
@@ -7,29 +7,42 @@ int main (int argc, char *argv[]) {
     int opc;
     int i=0;
     do{
-    opc = menu();
+        printf("\n----------MENU---------");
+        printf("\n1. Ingresar Libros. ");
+        printf("\n2. Mostrar Libros. ");
+        printf("\n3. Buscar Libro por Id. ");
+        printf("\n4. Buscar libro por Titulo.");
+        printf("\n5. Actualizar Estado de un Libro. ");
+        printf("\n6. Eliminar Libro. ");
+        printf("\n7. Salir. ");
+        printf("\nDigite que Opcion desea: ");scanf("%i", opc);
     switch (opc)
     {
         case 1:
-            registrarLibros(libros, i);
+            registrarLibros(libros);
             break;
         case 2:
             mostrarLibros(libros);
             break;
         case 3:
-            int id;
-            printf("Ingrese el id del libro a buscar: ");
-            scanf("%d", &id);
-            buscarLibroId(libros, id);
-            i++;
+            buscarLibroId(libros);
             break;
         case 4:
-            char titulo[100];
-            printf("Ingrese el titulo del libro a buscar: ");
-            scanf("%s", titulo);
-            buscarLibroTitulo(libros, titulo);
+            buscarLibroTitulo(libros);
             break;
+        case 5:
+            ActualizarLibro(libros);
+            break;
+        case 6:
+            EliminarLibro(libros); 
+        break;
+        case 7:
+            printf("\nSaliendo....");
+        break;
         
+        default:
+        printf("\nEsa Opcion no Existe N/A");
+        break;
 
     }
     }while(opc != 7);
